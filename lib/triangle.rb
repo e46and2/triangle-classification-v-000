d1556c654725937a584f0ab48109a7141ab3794d
Emily Nicholl Liepe <e.nicholl.liepe@gmail.com>
2019-02-13 20:16:28 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -19,16 +19,18 @@ class Triangle
       end
     else 
       if (@l1 == @l2 && @l2 == @l3 && @l3 > 0 && @l2 > 0 && @l1 > 0)
-      :equilateral
-    elsif (@l1 == @l2 || @l2 == @l3 || @l1 == @l3 && @l1 > 0 && @l3 > 0) 
-      :isosceles
-    elsif (@l1 + @l2 > @l3 || @l1 + @l3 > @l2 || @l2 + @l3 > @l1 && @l1 > 0 && @l2 > 0 && @l3 > 0 )
-      :scalene
+        :equilateral
+      elsif (@l1 == @l2 || @l2 == @l3 || @l1 == @l3 && @l1 > 0 && @l3 > 0) 
+        :isosceles
+      elsif (@l1 + @l2 > @l3 || @l1 + @l3 > @l2 || @l2 + @l3 > @l1 && @l1 > 0 && @l2 > 0 && @l3 > 0 )
+        :scalene
+      else 
         begin
         raise TriangleError
           rescue TriangleError => error
           puts error.message
       end
+    end
    end
 end
   
