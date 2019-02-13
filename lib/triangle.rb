f506ab85975c6e32a15d6efe3470b4db2bd73fbd
Emily Nicholl Liepe <e.nicholl.liepe@gmail.com>
2019-02-13 19:43:34 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,11 @@
 class Triangle
+  attr_accessor :l1, :l2, :l3, :equilateral, :isosceles, :scalene
   
+  def initialize(l1, l2, l3)
+    @l1 = l1
+    @l2 = l2
+    @l3 = l3
+    @equilateral = equilateral
+    @isosceles = isosceles
+    @scalene = scalene
 end
