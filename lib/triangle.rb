dfee5963916dca00cb7336f22880007a6d20363c
Emily Nicholl Liepe <e.nicholl.liepe@gmail.com>
2019-02-13 19:56:28 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,7 +15,7 @@ class Triangle
       @equilateral
     if @l1 == @l2 || @l2 == @l3 && @l1 != 0 && @l3 != 0 
       @isosceles
-    if @l1 + @l2 > @l3 || @l1 + @l3 > @l2 || @l2 + @l3 > @l1 
+    if @l1 + @l2 > @l3 || @l1 + @l3 > @l2 || @l2 + @l3 > @l1 && @l1 != 0 && @l2 != 0 && @l3 != 0 
       @scalene
     else 
       begin
