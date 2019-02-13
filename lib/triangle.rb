8f91a1b5cde29173fbc3df8e9ac44eba886a56f0
Emily Nicholl Liepe <e.nicholl.liepe@gmail.com>
2019-02-13 19:59:06 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,7 @@ class Triangle
   
   def kind 
     if @l1 == @l2 && @l2 == @l3 && @l3 != 0
-      @equilateral
+      return @equilateral
     elsif @l1 == @l2 || @l2 == @l3 && @l1 != 0 && @l3 != 0 
       @isosceles
     elsif @l1 + @l2 > @l3 || @l1 + @l3 > @l2 || @l2 + @l3 > @l1 && @l1 != 0 && @l2 != 0 && @l3 != 0 
