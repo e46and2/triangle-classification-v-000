569f5e59dd4f8786627e6787509f6daf0627014d
Emily Nicholl Liepe <e.nicholl.liepe@gmail.com>
2019-02-13 20:06:35 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,11 +18,7 @@ class Triangle
     elsif @l1 + @l2 > @l3 || @l1 + @l3 > @l2 || @l2 + @l3 > @l1 && @l1 > 0 && @l2 > 0 && @l3 > 0 
       :scalene
     else 
-      begin
         raise TriangleError
-      rescue TriangleError => error
-          puts error.message
-      end
    end
 end
   
