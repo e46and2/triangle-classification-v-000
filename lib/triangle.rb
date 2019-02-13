335def12c5a3143f13558a9d0901b811f230d734
Emily Nicholl Liepe <e.nicholl.liepe@gmail.com>
2019-02-13 20:21:58 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -16,29 +16,8 @@ class Triangle
   return :equilateral if a == c
   return :isosceles if a == b or b == c
   return :scalene
-end
+  end
   
-  def kind() 
-    if @l1 <= 0 || @l2 <= 0 || @l3 <=0 
-      begin
-        raise TriangleError
-          rescue TriangleError => error
-          puts error.message
-      end
-    elsif (@l1 == @l2 && @l2 == @l3 && @l3 > 0 && @l2 > 0 && @l1 > 0)
-        :equilateral
-    elsif (@l1 == @l2 || @l2 == @l3 || @l1 == @l3 && @l1 > 0 && @l3 > 0) 
-        :isosceles
-    elsif (@l1 + @l2 > @l3 || @l1 + @l3 > @l2 || @l2 + @l3 > @l1 && @l1 > 0 && @l2 > 0 && @l3 > 0 )
-        :scalene
-    else 
-        begin
-        raise TriangleError
-          rescue TriangleError => error
-          puts error.message
-      end
-   end
-end
   
   class TriangleError < StandardError
     def message
