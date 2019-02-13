94e8c3b1da059564f1413c10d9779d08bcd5dfe8
Emily Nicholl Liepe <e.nicholl.liepe@gmail.com>
2019-02-13 20:37:41 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -20,7 +20,7 @@ class Triangle
         :equilateral
       elsif (@l1 == @l2 || @l2 == @l3 || @l1 == @l3) 
         :isosceles
-      elsif (@l1 + @l2 > @l3 || @l1 + @l3 > @l2 || @l2 + @l3 > @l1 && @l1 > 0 && @l2 > 0 && @l3 > 0 )
+      elsif (@l1 + @l2 > @l3 || @l1 + @l3 > @l2 || @l2 + @l3 > @l1)
         :scalene
       else
         raise TriangleError
